ddeb63bb330c7936f1354dce72cc89ac6b8743f6
Vital Fadeev <vital.fadeev@gmail.com>
2019-10-29 00:38:09 +0700

    * fixes

---
+++ _dev_scripts/multiprocessing_reader.py
@@ -0,0 +1,26 @@
+import itertools
+from multiprocessing import Pool
+from time import sleep
+
+
+def f( x ):
+    print( "f()" )
+    sleep( 3 )
+    return x
+
+
+def get_reader():
+    for x in range( 10 ):
+        print( "readed: ", x )
+        value = " " * 1024 * 64 # 64k
+        yield value
+
+
+if __name__ == '__main__':
+    p = Pool( processes=2 )
+
+    data = p.imap( f, get_reader() )
+
+    p.close()
+    p.join()
+
