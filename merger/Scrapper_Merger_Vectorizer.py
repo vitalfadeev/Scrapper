da07a5878fd1ed334a53ab34be9a1269fe883921
Vital Fadeev <vital.fadeev@gmail.com>
2019-11-11 13:02:39 +0700

    * vectorizer

--- merger/Scrapper_Merger_Vectorizer.py
+++ merger/Scrapper_Merger_Vectorizer.py
@@ -3,6 +3,7 @@ import logging
 
 from Scrapper_DB import DBRead, DBExecute
 from Scrapper_IxiooAPI import Vectorize_database_record, Vectorize_PKS
+from merger.Scrapper_Merger_DB import DBWord
 from wikidata.Scrapper_Wikidata_DB import DBWikidata
 from wikipedia.Scrapper_Wikipedia_DB import DBWikipedia
 from wiktionary.Scrapper_Wiktionary_DB import DBWiktionary
@@ -122,9 +123,61 @@ def vectorize_properties_wikidata():
 
 
 
+def vectorize_properties_words():
+    log.info( "Vectorizing words" )
+
+    for w in DBRead( DBWord, sql=" SELECT * FROM words ", cls=dict ):
+
+        log.info( "  vectorize: %s", w["LabelName"] )
+
+        vetorized = Vectorize_database_record( w )
+
+        DBExecute( DBWord, """
+            UPDATE words 
+               SET
+                   ExplainationTxt_Vect = ?,
+                   AlternativeFormsOther_Vect = ?,
+                   Synonymy_Vect = ?,
+                   Antonymy_Vect = ?,
+                   Hypernymy_Vect = ?,
+                   Hyponymy_Vect = ?,
+                   Meronymy_Vect = ?,
+                   RelatedTerms_Vect = ?,
+                   Coordinate_Vect = ?,
+                   Otherwise_Vect = ?,
+
+                   Description_Vect = ?,
+                   AlsoKnownAs_Vect = ?,
+                   Instance_of_Vect= ?,
+                   Subclass_of_Vect = ?,
+                   Part_of_Vect = ?,
+
+                   Operation_Vectorizer = 1
+             WHERE PrimaryKey = ?
+             """,
+                to_json( vetorized["ExplainationTxt"] ),
+                to_json( vetorized["AlternativeFormsOther"] ),
+                to_json( vetorized["Synonymy"] ),
+                to_json( vetorized["Antonymy"] ),
+                to_json( vetorized["Hypernymy"] ),
+                to_json( vetorized["Hyponymy"] ),
+                to_json( vetorized["Meronymy"] ),
+                to_json( vetorized["RelatedTerms"] ),
+                to_json( vetorized["Coordinate"] ),
+
+                to_json( vetorized["Description"] ),
+                to_json( vetorized["AlsoKnownAs"] ),
+                to_json( vetorized["Instance_of"] ),
+                to_json( vetorized["Subclass_of"] ),
+                to_json( vetorized["Part_of"] ),
+
+                w["PrimaryKey"]
+            )
+
+
+
+
 def vectorize_properties():
     log.info( "Vectorizing" )
-    vectorize_properties_wikipedia()
-    #vectorize_properties_wiktionary()
-    #vectorize_properties_wikidata()
+    vectorize_properties_words()
 
